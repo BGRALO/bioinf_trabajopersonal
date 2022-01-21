10803a699dcc3bca655a48006946c908bc5e1d6a
BGRALO <borjagracia9@gmail.com>
2022-01-21 10:40:50 +0100

    Cambios de final de clase comandos aggregate

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ## Exportar csv
 
 Hemos exportado un csv llamado *script_exportar.r*
+
+## Calculos
+Hemos llevado a vcabo una serie de cambios mediante la utilizacion de una serie de comandos
--- datasettiroides.R
+++ datasettiroides.R
@@ -2,3 +2,23 @@ library(MLDataR)
 tir <- MLDataR::thyroid_disease
 
 write.csv(tir, file = "dataset_tiroides.csv")
+
+sessionInfo()
+# order --> es para ordenar (como sort pero sort para vectores, order para bases de datos )
+head(tir)
+
+sort(tir$patient_age)
+
+tir2= tir[order(tir$patient_age),]
+
+tir_completo = tir[complete.cases(tir),] # complete.cases--> para quitar todos los pacientes que tengan informacion misses
+
+# aggregate --> paar agurpar por grupos y hacer calculos estadísticos 
+edad_tyr = aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass), mean)
+
+names(edad_tyr)= c("Tiroides", "Media")
+
+aggregate(tir_completo$patient_age, list(tir_completo$ThryroidClass, tir_completo$patient_gender), mean)
+
+
+
